bb6b4d8a3c2a80e533053cf249972bcd05033bad
Ben Yang <benyang2020@gmail.com>
2021-08-31 03:17:20 -0400

    commit tree 6685592a5270c650761174ca141ce0b2a2db735f

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400) {
+    return "This one is on me!"
+  }else if (someValue <= 2499){
+    return 'I will gladly take your thirty bucks.'
+  }else if (someValue >= 2500) {
+    return 'No can do.'
+  }
+
+  
+
+  }
+  
+
+
+function ternaryCheckCity(destination){
+  // Write your code here!
+  if (destination === "NYC") {
+    return "Ok, sounds good."
+  } else{
+    return "No go."
+  } 
+
+
+
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+ 
+  switch (tip) {
+    case 'generous': 
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default: 
+      return 'Bye.'
+  }
+
 }
